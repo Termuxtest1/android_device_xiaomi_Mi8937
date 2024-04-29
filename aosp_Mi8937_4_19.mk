584db2cf63fc88b8269ab15046ebbb5841e04efc
Termux <89096430+Termuxtest1@users.noreply.github.com>
2024-04-29 18:29:53 +0530

    Bringup Test1 Matrix:A14

--- aosp_Mi8937_4_19.mk
+++ aosp_Mi8937_4_19.mk
@@ -13,6 +13,16 @@ $(call inherit-product, device/xiaomi/Mi8937/device.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
+# Call the BCR setup
+$(call inherit-product-if-exists, vendor/bcr/bcr.mk)
+
+WITH_GMS := false
+
+MATRIXX_BUILD_TYPE := Unofficial
+MATRIXX_MAINTAINER := Termuxtest1 
+
+TARGET_ENABLE_BLUR := true
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := Mi8937_4_19
 PRODUCT_NAME := aosp_Mi8937_4_19
@@ -23,6 +33,11 @@ PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+#Add Google Contacts, Dialer & Messaging 
+BUILD_GOOGLE_CONTACTS := true
+BUILD_GOOGLE_DIALER := true
+BUILD_GOOGLE_MESSAGE := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="land-user 6.0.1 MMB29M V10.2.2.0.MALMIXM release-keys"
